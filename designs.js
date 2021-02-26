fa14af20d53f974076bce56bbc1f82009c389d14
ChristaPigeon <cstath26@gmail.com>
2021-02-26 08:15:02 -0800

    feat: Assign variables to DOM elements

--- designs.js
+++ designs.js
@@ -1,5 +1,12 @@
+// Assign variables
+// Select canvas
+const pixelCanvas = document.getElementById('pixelCanvas');
+
 // Select color input
+const color = document.getElementById('colorPicker');
+
 // Select size input
+const sizePicker = document.getElementById('sizePicker');
 
 // When size is submitted by the user, call makeGrid()
 
