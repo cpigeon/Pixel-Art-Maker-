82e8dc7e4f26301b54b35f3253ad23593038c7af
ChristaPigeon <cstath26@gmail.com>
2021-02-26 08:19:59 -0800

    feat: Write makeGrid function

--- designs.js
+++ designs.js
@@ -10,8 +10,14 @@ const sizePicker = document.getElementById('sizePicker');
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
+function makeGrid(height, width) {
 // Your code goes here!
-
+  for (n = 0; n < height; n++) {
+    const newRow = document.createElement('tr');
+    pixelCanvas.appendChild(newRow);
+    for (m = 0; m < width; m++) {
+      const newCell = document.createElement('td');
+      newRow.appendChild(newCell);
+    }
+  }
 }
